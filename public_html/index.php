594254cb7c538921e20c5f326ed1bdfed2605129
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-11-28 11:14:50 +0200

    added maps iframe, finalized static index.php parts

--- public_html/index.php
+++ public_html/index.php
@@ -64,6 +64,9 @@ if (!App::$session->userLoggedIn()) {
             </div>
         </div>
     </section>
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.219602278381!2d25.33569661506837!3d54.72335198029061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0xdd7887e198efd4c7!2sSaul%C4%97tekio%20al.%2015%2C%20Vilnius%2010221!5e0!3m2!1slt!2slt!4v1574932159096!5m2!1slt!2slt"
+            width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 </main>
 
 <footer>
